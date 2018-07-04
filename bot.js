46782f44a9e2206b928f399626e88dff6c05fbeb
luigisteve21 <luigisteve21@live.com>
2018-07-03 20:02:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () == {
+client.on('ready', () => {
     console.log("I am ready!'); 
 });
 
-client.on('message', message == {
+client.on('message', message => {
     if (message.content === 'ping') {
        message.reply('pong');
     }
